29e476c4de8ffd93d1bbe2bd6b14870ea8d014ee
Tobias Wälde <t.waelde@pm.me>
2026-09-07 19:40:19 +0200

    chore: bootstrap bridge application

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  collectCoverageFrom: ['src/**/*.ts'],
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: { '^.+\\.(t|j)s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json' }] },
+  testEnvironment: 'node',
+  moduleNameMapper: { '^~/(.*)$': '<rootDir>/src/$1' },
+};
+
+export default config;
---
+++ src/config/env.ts
@@ -0,0 +1,20 @@
+import * as dotenv from 'dotenv';
+import { cleanEnv, num, str, url } from 'envalid';
+
+dotenv.config();
+
+const environment = cleanEnv(process.env, {
+  CORS_ORIGIN: str({ default: '*' }),
+  HOST: str({ default: '0.0.0.0' }),
+  MQTT_CLIENT_ID: str({ default: '' }),
+  MQTT_HOST: str(),
+  MQTT_PASSWORD: str({ default: undefined }),
+  MQTT_PORT: num({ default: 1883 }),
+  MQTT_PROTOCOL: str({ choices: ['mqtt', 'mqtts'], default: 'mqtt' }),
+  MQTT_USERNAME: str({ default: undefined }),
+  POCKETBASE_API_KEY: str(),
+  POCKETBASE_URL: url(),
+  PORT: num({ default: 3000 }),
+});
+
+export const ENV = environment;
